f8ab2d45f567fc5dbe69188e54fac509dc18e748
Mike Moss <mrmoss@alaska.edu>
2013-08-21 14:05:04 -0800

    Windows working with spaces now...

--- compiler/compile_scripts/windows.bat
+++ compiler/compile_scripts/windows.bat
@@ -1,7 +1,7 @@
 @echo off
-set CPP_COMPILER="%2\..\..\mingw\bin\g++.exe"
-set SRC="%4"
-set OBJECT_FILES="%2\*.o"
+set CPP_COMPILER=%2\..\..\mingw\bin\g++.exe
+set SRC=%4
+set OBJECT_FILES=%2\*.o
 set LIB=-lglu32 -lopengl32 -lwsock32 -lglaux -lgdi32 -lwinmm -lpthread
 set CFLAGS=-static -fpermissive -w -Wno-write-strings -Wno-unused-result -Wno-format -O -static -mwindows -DGLEW_BUILD -DGLEW_STATIC -DFREEGLUT_BUILD -DFREEGLUT_STATIC
 set INCLUDES=-I%3 -I%3/gk12 -I%3/glui -I%3/glut

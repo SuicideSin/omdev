d9532f7041e84e0c23480ed4328690fe108f2d1f
Mike Moss <mrmoss@alaska.edu>
2013-08-21 12:19:30 -0800

    Fixed linux script to work with directories with spaces...

--- compiler/compile_scripts/linux.sh
+++ compiler/compile_scripts/linux.sh
@@ -1,9 +1,17 @@
+i=1
+
+for args in "$@"; do
+  arg[$i]="$args"
+  ((i++))
+done
+
 CPP_COMPILER="g++"
-SRC="$4"
-OBJECT_FILES="$2/*.o"
+SRC="${arg[4]}"
+OBJECT_FILES="${arg[2]}/*.o"
 LIB="-lGL -lGLU -lX11"
 CFLAGS="-fpermissive -w -Wno-write-strings -Wno-unused-result -Wno-format -O"
-INCLUDES="-I$3 -I$3/gk12 -I$3/glut"
-OUT="-o $5"
+INCLUDES="-I${arg[3]} -I${arg[3]}/gk12 -I${arg[3]}/glut"
+OUT="${arg[5]}"
 
-${CPP_COMPILER} ${SRC} ${OBJECT_FILES} ${LIB} ${CFLAGS} ${INCLUDES} ${OUT} 2> $1
+
+${CPP_COMPILER} "${SRC}" ${OBJECT_FILES} ${LIB} ${CFLAGS} ${INCLUDES} -o "${OUT}" 2> ${arg[1]}

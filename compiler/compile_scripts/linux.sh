938bfffda1dc26226bd257516e6966e246dd6554
Mike Moss <mrmoss@alaska.edu>
2013-08-21 14:45:49 -0800

    Linux works again...

--- compiler/compile_scripts/linux.sh
+++ compiler/compile_scripts/linux.sh
@@ -1,17 +1,17 @@
 i=1
 
 for args in "$@"; do
-  arg[$i]="$args"
-  ((i++))
+	arg[$i]="$args"
+	((i++))
 done
 
-CPP_COMPILER="g++"
-SRC="${arg[4]}"
-OBJECT_FILES="${arg[2]}/*.o"
-LIB="-lGL -lGLU -lX11"
-CFLAGS="-fpermissive -w -Wno-write-strings -Wno-unused-result -Wno-format -O"
-INCLUDES="-I${arg[3]} -I${arg[3]}/gk12 -I${arg[3]}/glut"
-OUT="${arg[5]}"
+#CPP_COMPILER="g++"
+#SRC="${arg[4]}"
+#OBJECT_FILES="${arg[2]}/*.o"
+#LIB="-lGL -lGLU -lX11"
+#CFLAGS="-fpermissive -w -Wno-write-strings -Wno-unused-result -Wno-format -O"
+#INCLUDES=-I"${arg[3]}" -I"${arg[3]}/gk12" -I"${arg[3]}/glut"
+#OUT=-o "${arg[5]}"
 
-
-${CPP_COMPILER} "${SRC}" ${OBJECT_FILES} ${LIB} ${CFLAGS} ${INCLUDES} -o "${OUT}" 2> ${arg[1]}
+g++ "${arg[4]}" "${arg[2]}"/*.o -lGL -lGLU -lX11 -fpermissive -w -Wno-write-strings -Wno-unused-result -Wno-format \
+-O -I"${arg[3]}" -I"${arg[3]}/gk12" -I"${arg[3]}/glut" -o "${arg[5]}" 2> "${arg[1]}"
